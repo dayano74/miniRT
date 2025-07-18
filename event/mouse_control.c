3f14d80b253870b08b06d92b25ce0bd312cefea8
dayano74 <kiixxn00@gmail.com>
2025-07-18 20:31:19 +0900

    add mouse wheel control

--- event/mouse_control.c
+++ event/mouse_control.c
@@ -6,11 +6,44 @@
 /*   By: dayano <dayano@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 19:26:41 by dayano            #+#    #+#             */
-/*   Updated: 2025/07/18 20:02:36 by dayano           ###   ########.fr       */
+/*   Updated: 2025/07/18 20:29:49 by dayano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minirt.h"
+
+// static void	zoom_camera(t_world *world, double zoom_factor)
+// {
+// 	double	prev_fov;
+
+// 	prev_fov = world->camera->fov;
+// 	world->camera->fov = world->camera->fov / zoom_factor;
+
+// 	if (world->camera->fov < 10.0)
+// 		world->camera->fov = 10.0;
+// 	else if (world->camera->fov > 120.0)
+// 		world->camera->fov = 120.0;
+// 	if (world->camera->fov == prev_fov)
+// 		return;
+// }
+
+static void	zoom_camera(t_world *world, double zoom_factor)
+{
+	t_vec	dir;
+	double	move_distance;
+
+	dir.x = world->camera->dir.x;
+	dir.y = world->camera->dir.y;
+	dir.z = world->camera->dir.z;
+	vec_normalize(dir);
+	if (zoom_factor > 1.0)
+		move_distance = 0.5;
+	else
+		move_distance = -0.5;
+	world->camera->pos.x += dir.x * move_distance;
+	world->camera->pos.y += dir.y * move_distance;
+	world->camera->pos.z += dir.z * move_distance;
+}
 
 int	mouse_press(int button, int x, int y, t_world *world)
 {
@@ -20,6 +53,20 @@ int	mouse_press(int button, int x, int y, t_world *world)
 		world->input.last_mouse_x = x;
 		world->input.last_mouse_y = y;
 		world->input.mouse_initialized = true;
+	}
+	else if (button == 4)
+	{
+		zoom_camera(world, 1.5);
+		world->mosaic_size = 21;
+		raytracing(world);
+		mlx_put_image_to_window(world->mlx, world->win, world->img, 0, 0);
+	}
+	else if (button == 5)
+	{
+		zoom_camera(world, 0.5);
+		world->mosaic_size = 21;
+		raytracing(world);
+		mlx_put_image_to_window(world->mlx, world->win, world->img, 0, 0);
 	}
 	return (0);
 }
