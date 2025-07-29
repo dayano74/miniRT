9a1d288319e1a8287be4625f7aa01bd81085650b
Okuda Kaname <okaname@c6r1s4.42tokyo.jp>
2025-07-29 17:03:41 +0900

    not allow minus-scale

--- parser/make_object/make_cylinder.c
+++ parser/make_object/make_cylinder.c
@@ -6,11 +6,20 @@
 /*   By: okaname <okaname@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 18:54:46 by okaname           #+#    #+#             */
-/*   Updated: 2025/07/05 18:03:16 by okaname          ###   ########.fr       */
+/*   Updated: 2025/07/29 14:45:14 by okaname          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minirt.h"
+
+static void	set_obj(t_world **world, t_obj *obj)
+{
+	if ((*world)->objects == NULL)
+		(*world)->objects = obj;
+	else
+		(*world)->last_objects->next = obj;
+	(*world)->last_objects = obj;
+}
 
 void	make_cylinder(char **tokenlist, t_world *world)
 {
@@ -25,18 +34,16 @@ void	make_cylinder(char **tokenlist, t_world *world)
 		error_malloc(tokenlist, world);
 	cylinder.pos = token_to_vec(tokenlist[1]);
 	cylinder.axis = token_to_vec(tokenlist[2]);
-	cylinder.dia = ft_atof(tokenlist[3]);
-	cylinder.height = ft_atof(tokenlist[4]);
+	if (!atof_with_error(tokenlist[3], INT_MAX, 0, &cylinder.dia))
+		error_invalid(tokenlist, world);
+	if (!atof_with_error(tokenlist[4], INT_MAX, 0, &cylinder.height))
+		error_invalid(tokenlist, world);
 	if (!token_to_color(tokenlist[5], &color))
 		return (free(obj), error_invalid(tokenlist, world));
 	cylinder.color = color_normalize(color);
 	obj->type = CYLINDER;
 	obj->u_object.cylinder = cylinder;
 	obj->next = NULL;
-	if (world->objects == NULL)
-		world->objects = obj;
-	else
-		world->last_objects->next = obj;
-	world->last_objects = obj;
+	set_obj(&world, obj);
 	world->set_obj |= 4;
 }
